f2185522b6cb98d652231a84a845fe884bbfb0b1
Milo <jerezemilio@hotmail.com>
2022-05-06 10:47:40 -0300

    Componentes

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  //Esto es para generar un componente inline
+  //template:"<p>Aqui va empleado</p>",
+  styleUrls: ['./empleado.component.css']
+  //styles: ["p{background-color:red;}"]
+
+})
+export class EmpleadoComponent implements OnInit {
+
+  nombre="Juan";
+  apellido="Perez";
+  edad=21;
+  //empresa="Tucuman";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  llamaEmpresa(value:string){
+    
+  }
+
+}
